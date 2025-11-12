d7e67af9640b4de97c6ffd89d91275b650ff5255
Sajad Nowroozi <0799111111aminy11@gmail.com>
2025-11-12 21:19:42 +0430

    End of DAY 8 methods

---
+++ Day 8/MethodsInJava.java
@@ -0,0 +1,46 @@
+class MethodsInJava{
+
+// Simple Method
+
+        public static void printName(){
+            System.out.println("Your name is sajad");
+        }
+
+
+
+
+// Method with  one Parameters
+
+        public static void printName2(String name){
+             System.out.println("Your name is "+name);
+        }
+
+
+
+
+// Multi-parameter method
+
+        public static void  printName3(String name, String lname , int age, String phone){
+            System.out.println("Hello "+name+" "+lname+".\nYou are "+age+" Years old.\n"+"your phone number is "+phone+".");
+        }
+
+
+
+
+// Example of Return Type Method return sum of two numbers.
+
+        public static int  sumNumber(int num1 , int num2){
+            int total = num1+num2;
+            return total;
+        }
+
+
+
+
+
+
+
+
+
+}
+
